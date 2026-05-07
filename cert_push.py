3e433e91cd273da579e755e3f80f8def292b29cb
JollyPropoganda <74634228+JollyPropoganda@users.noreply.github.com>
2026-05-07 15:14:13 +0800

    commiting sixth rendition of cert_push.py change: - changed date time handling to handle different zones

--- cert_push.py
+++ cert_push.py
@@ -3,7 +3,6 @@ import os
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 import subprocess
-import datetime
 from datetime import datetime, timedelta, timezone
 
 # Configuration
